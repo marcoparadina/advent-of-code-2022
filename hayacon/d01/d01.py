e85a5a0cbc0cc4750b6556d818dcc8e527b2e307
Hayato Conrad Ishida <hayatoishida@Hayatos-MacBook-Pro.local>
2022-12-02 00:18:27 +0100

    day 01

--- hayacon/d01/d01.py
+++ hayacon/d01/d01.py
@@ -2,22 +2,18 @@
 
 f = open('input.txt', 'r')
 
-theMostCalories = 0
 totalCalories = 0
+elvesCalories = []
 
 lines = f.readlines()
 for line in lines:
     if line == '\n':
-        if totalCalories > theMostCalories:
-            theMostCalories = totalCalories
-        else:
-            theMostCalories = theMostCalories
+        elvesCalories.append(totalCalories)
         totalCalories = 0
         continue
     else:
         totalCalories += int(line)
 
-print(theMostCalories)
-
-
+elvesCalories.sort(reverse=True)
+print(elvesCalories[0] + elvesCalories[1] + elvesCalories[2])
 f.close()
